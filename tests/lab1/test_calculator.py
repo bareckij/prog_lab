251248a20995314dc58faae478c431fe01096e86
bareckij <bareckijmaksim@gmail.com>
2024-10-07 18:15:51 +0300

    fixed some errors

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -4,10 +4,10 @@ class CalculatorTestCase(unittest.TestCase):
 
 
     def test_add(self):
-        self.assertEqual(calc.add(self, 2, 3 ), 5)
+        self.assertEqual(calc.add(self, 2, 3 ), 5.0)
     def test_sub(self):
-        self.assertEqual(calc.sub(self, 10, 5), 5)
+        self.assertEqual(calc.sub(self, 10, 5), 5.0)
     def test_mltpl(self):
-        self.asssertEqual(calc.mltpl(self, 2, 3), 6)
+        self.assertEqual(calc.mltpl(self, 2, 3), 6.0)
     def test_div(self):
-        self.assertEqual(calc.div(10, 2), 5)
+        self.assertEqual(calc.div(self, 10, 2), 5.0)
