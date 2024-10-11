ad0cc30d90cb6ed31c2383ec9d82619c811d0b1d
bareckij <bareckijmaksim@gmail.com>
2024-10-11 14:52:02 +0300

    changed class name

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -2,10 +2,10 @@ import unittest
 from src.lab1.calculator import _calc
 class CalculatorTestCase(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(calc.add(self, 2, 3 ), 5.0)
+        self.assertEqual(_calc.add(self, 2, 3 ), 5.0)
     def test_sub(self):
-        self.assertEqual(calc.sub(self, 10, 5), 5.0)
+        self.assertEqual(_calc.sub(self, 10, 5), 5.0)
     def test_mltpl(self):
-        self.assertEqual(calc.mltpl(self, 2, 3), 6.0)
+        self.assertEqual(_calc.mltpl(self, 2, 3), 6.0)
     def test_div(self):
-        self.assertEqual(calc.div(self, 10, 2), 5.0)
+        self.assertEqual(_calc.div(self, 10, 2), 5.0)
