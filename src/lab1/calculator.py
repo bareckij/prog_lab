5186df2023e55e5954789d8293e43265656ace45
bareckij <bareckijmaksim@gmail.com>
2024-10-11 14:26:07 +0300

    added doctrings

--- src/lab1/calculator.py
+++ src/lab1/calculator.py
@@ -1,30 +1,59 @@
-class calc: 
-    def add(self, a, b):
-        return (a + b)
-    def sub(self, a, b): 
-        return (a - b)   
-    def mltpl(self, a, b):
-        return (a * b) 
-    def div(self, a, b):
-        try: 
-            return (a/b)
+class _calc:
+    """Class _calc so it can be imported into other python files...."""
+
+    def add(self, first_number, second_number):
+        """
+        Function to add two numbers.
+        Take value from variables first_number and second_number.
+        """
+        return first_number + second_number
+
+    def sub(self, first_number, second_number):
+        """
+        Function to substract one number from another.
+        Take value from variables first_number and second_number.
+        """
+        return first_number - second_number
+
+    def mltpl(self, first_number, second_number):
+        """
+        Function to multiply two numbers.
+        Take value from variables first_number and second_number.
+        """
+        return first_number * second_number
+
+    def div(self, first_number, second_number):
+        """
+        Function to divide one number by another.
+        Take value from variables first_number and second_number.
+        """
+        try:
+            return first_number / second_number
         except ZeroDivisionError:
             return "You can't divide by zero"
 
+
 def main():
-    calculator = calc()
+    """
+    Main function.
+    Takes two variables, operation and print calculations to terminal.
+    Also creates exemple variable.
+    """
+    calculator = _calc()
     while True:
-        a = float(input('Enter first number: '))
-        b = float(input('Enter second number: '))
-        choice = input('Choose operation(+, -, *, /): ')
-        match(choice):
-            case '+':
-                print(calculator.add(a, b))
-            case '-':
-                print(calculator.sub(a, b))
-            case '*':
-                print(calculator.mltpl(a, b))
-            case '/':
-                print(calculator.div(a, b))
+        first_number = float(input("Enter first number: "))
+        second_number = float(input("Enter second number: "))
+        choice = input("Choose operation(+, -, *, /): ")
+        match (choice):
+            case "+":
+                print(calculator.add(first_number, second_number))
+            case "-":
+                print(calculator.sub(first_number, second_number))
+            case "*":
+                print(calculator.mltpl(first_number, second_number))
+            case "/":
+                print(calculator.div(first_number, second_number))
+
+
 if __name__ == "__main__":
     main()
